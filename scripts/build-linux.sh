46cbb6fd50b05bd2a7c3ea7ac6c55ff497a7031a
Steven Hildreth <steven@hixi.org>
2025-06-06 16:58:39 -0500

    milestone commit

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -6,19 +6,21 @@ set -e
 # 1. Build React UI
 print '\n[1/4] Building React UI...'
 cd ../src/ui
-npm install
+rm -rf node_modules package-lock.json
+npm cache clean --force
+npm install --verbose
 npm run build
 
 # 2. Install Electron dependencies
 print '\n[2/4] Installing Electron dependencies...'
 cd ../electron
-npm install
+rm -rf node_modules package-lock.json
+npm cache clean --force
+npm install --verbose
 
 # 3. Ensure electron-builder is installed
-if ! npx electron-builder --version > /dev/null 2>&1; then
-  print '\n[3/4] Installing electron-builder...'
-  npm install --save-dev electron-builder
-fi
+print '\n[3/4] Installing electron-builder...'
+npm install --save-dev electron-builder
 
 # 4. Build Electron App for Linux only
 print '\n[4/4] Building Electron App for Linux (AppImage, rpm, deb, snap, pacman, tar.gz)...'
